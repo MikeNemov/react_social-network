f2b7c93c6fbbb8a3105a49894d2e6dfc17009965
Mike <headmn@gmail.com>
2021-08-09 10:54:24 +0300

    hw2_02

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
 import {Message} from './Message';
+import {useState, useEffect} from 'react';
+import styles from './css/message.module.sass';
 
-const myName = 'Michail';
 
 export function App() {
+
+    const [text, setText] = useState('');
+    const [author, setAuthor] = useState('');
+
+    const [messages, setMessages] = useState([]);
+
+    const handleSendMessage = () => {
+        if (!author || !text) {
+            return;
+        }
+        setMessages((state) => [...state, {id: new Date().getTime(), author: author, text: text}]);
+        setAuthor('');
+        setText('');
+
+    };
+    useEffect(() => {
+        if (messages.length === 0 || messages[messages.length - 1].author === 'Vasily') {
+            return;
+        }
+        setTimeout(() => {
+            setMessages([...messages, {
+                id: new Date().getTime(),
+                author: `Vasily`,
+                text: `Hi, i'm bot`
+            }]);
+
+        }, 1500);
+
+
+    }, [messages]);
+
+    const onFormSubmit = e => {
+        e.preventDefault();
+        handleSendMessage();
+    };
+
     return (
-        <div className="App">
-            <Message name={myName}/>
+        <div>
+            {messages.map(message =>
+                <Message key={message.id} text={message.text} author={message.author}/>
+            )}
+
+
+            <form className={styles.message_form} onSubmit={onFormSubmit}>
+                <input
+                    value={author}
+                    placeholder='Author'
+                    onChange={(e) => setAuthor(e.target.value)}/>
+                <input
+                    value={text}
+                    type="text"
+                    placeholder='Message'
+                    onChange={(e) => setText(e.target.value)}/>
+                <button type="submit">Send
+                </button>
+            </form>
         </div>
     );
+
 }
 
 
